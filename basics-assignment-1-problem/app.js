e881db9c543c1a29be7777e35ee1b036c65499a9
tgoodell <github@tgoodell.com>
2022-05-18 09:10:14 -0400

    Completed basics assignment #1

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: 'Tristan',
+            age: 19,
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2.wp.com%2Flisaeatsworld.flywheelsites.com%2Fwp-content%2Fuploads%2F2013%2F11%2Fdsc_14592.jpg&f=1&nofb=1',
+        }
+    },
+})
+
+app.mount('#app')
