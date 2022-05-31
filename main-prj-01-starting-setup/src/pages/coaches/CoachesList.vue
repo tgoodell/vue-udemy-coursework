347127e88955f754b1ec5addea337c2d4ac9a0cd
tgoodell <github@tgoodell.com>
2022-05-31 09:02:32 -0400

    Lots of progress on main app,

---
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,52 @@
+<template>
+    <base-card>
+        <section>
+            FILTER
+        </section>
+        <section>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button>
+                <base-button link to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item v-for="coach in filteredCoaches" 
+                    :key="coach.id" 
+                    :id="coach.id" 
+                    :first-name="coach.firstName" 
+                    :last-name="coach.lastName" 
+                    :rate="coach.hourlyRate"
+                    :areas="coach.areas">
+                </coach-item>
+            </ul>
+            <h3 v-else>No coaches found.</h3>
+        </section>
+    </base-card>
+</template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+export default {
+  components: { CoachItem},
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches'];
+        },
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        CONTACT COACH
+    </div>
+</template>
